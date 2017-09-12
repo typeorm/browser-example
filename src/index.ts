07edbac661ecfeb6d110b9b9fb77ba5c5ab83b2d
Daniel Lang <ldaniell14260@gmail.com>
2017-09-12 22:05:23 +0200

    updated typeorm version, made model more complex

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
 import {createConnection} from "typeorm";
+import {Author} from "./entity/Author";
 import {Post} from "./entity/Post";
 import {Category} from "./entity/Category";
 
 createConnection({
-    driver: {
-        type: "websql",
-        database: "test",
-        extra: {
-            version: 1,
-            description: "test database",
-            size: 2 * 1024 * 1024
-        }
+    type: "websql",
+    database: "test",
+    extra: {
+        version: 1,
+        description: "test database",
+        size: 2 * 1024 * 1024
     },
     entities: [
+        Author,
         Post,
         Category
     ],
-    logging: {
-        logQueries: true
-    },
+    logging: true,
     autoSchemaSync: true
 }).then(async connection => {
 
@@ -28,10 +26,14 @@ createConnection({
     const category2 = new Category();
     category2.name = "Programming";
 
+    const author = new Author();
+    author.name = "Person";
+
     const post = new Post();
     post.title = "Control flow based type analysis";
     post.text = `TypeScript 2.0 implements a control flow-based type analysis for local variables and parameters.`;
     post.categories = [category1, category2];
+    post.author = author;
 
     const postRepository = connection.getRepository(Post);
     await postRepository.persist(post);
