fa5f44b013b321ec68377af03abb3fbd0ac29437
Umed Khudoiberdiev <pleerock.me@gmail.com>
2017-11-03 02:07:54 +0500

    Update Category.ts

--- src/entity/Category.ts
+++ src/entity/Category.ts
@@ -3,7 +3,7 @@ import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
 @Entity()
 export class Category {
 
-    @PrimaryGeneratedColumn("uuid")
+    @PrimaryGeneratedColumn()
     id: number;
 
     @Column()
